605204d07efca0cca127c5de3e7438a0c56f8876
Gumilin Aleksandr <gumilin.aleksandr@gmail.com>
2025-04-30 15:18:12 +0300

    repo

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,1 @@
+// https://681211573ac96f7119a6b20b.mockapi.io/
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsSlice } from "./contactsSlice";
 import { filtersSlice } from "./filtersSlice";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "contacts",
-  version: 1,
-  storage,
-};
-
-const persistConfigFilter = {
-  key: "filter",
-  version: 1,
-  storage,
-};
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(persistConfig, contactsSlice),
-    filters: persistReducer(persistConfigFilter, filtersSlice),
+    contacts: contactsSlice,
+    filters: filtersSlice,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export let persistor = persistStore(store);
